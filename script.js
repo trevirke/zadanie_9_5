19892afc52f4a7b717e1569de03d3c57ea492b81
Sylwia Mamczur <sylwia.mamczur@gmail.com>
2017-12-04 17:45:44 +0100

    Add files

---
+++ script.js
@@ -0,0 +1,8 @@
+var itemClass = document.getElementsByClassName('button');
+
+var lenght = itemClass.length;
+
+for (var i=0; i<lenght; i++) {
+//    var text = itemClass[i].innerText;    
+    console.log(itemClass[i].innerText);    
+}
